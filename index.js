c332e3d4d32a6b67535a4803b0d9c9d092245553
Archangel <nathapcal@gmail.com>
2020-09-23 22:46:37 -0400

    installed requirer

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 // array of questions for user
 const questions = [];
-const fs = require("fs")
-const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+const inquirer = require("inquirer");
 
+// inquirer prompts
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init() {}
 
 // function call to initialize program
 init();
-
 
 // console.log(generateMarkdown.test())
