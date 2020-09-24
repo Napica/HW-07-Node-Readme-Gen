54709ad4c3fedae66c87701af91a5085b1b2a0b9
Archangel <nathapcal@gmail.com>
2020-09-23 23:46:36 -0400

    added questions to the inquirer object array.  tested passed

--- index.js
+++ index.js
@@ -6,15 +6,72 @@ const inquirer = require("inquirer");
 
 // inquirer prompts
 
-inquirer.prompt ([
+inquirer
+  .prompt([
+    // Project Title
     {
-        type: "input",
-        message: "What would you like the project to be called?",
-        name: "projectTitle"
-    }
-]).then(function(response){
-    console.log(response);
-})
+      type: "input",
+      message: "What would you like the project to be called?",
+      name: "projectTitle",
+    },
+    // Project Description
+    {
+      type: "input",
+      message: "Please enter a brief description of the current project.",
+      name: "projectDescription",
+    },
+    // Project Installation Instructions
+    {
+      type: "input",
+      message:
+        "Please enter the installation process for this current project.  *note: Please use ",
+      name: "projectInstall",
+    },
+    // Project Usage Information
+    {
+      type: "input",
+      message:
+        "Please enter any usage information that was used for this project.",
+      name: "projectUsage",
+    },
+    {
+      type: "input",
+      message:
+        "Please enter the current guidelines on how to contribute to the current project.",
+      name: "projectHowToContribute",
+    },
+    {
+      type: "input",
+      message:
+        "Please enter any details on individuals or entities that have contributed to this current project.",
+      name: "projectContributors",
+    },
+    {
+      type: "input",
+      message:
+        "Please enter how a contributor can go about testing this current project.",
+      name: "projectTest",
+    },
+    {
+      type: "list",
+      message: "Please choose from the list of applicable licenses below: ",
+      choices: ["MIT", "Mozilla", "Apache", "Eclipse"],
+      name: "projectLicense",
+    },
+    {
+      type: "input",
+      message: "Please enter your GitHub username for this current project.",
+      name: "projectUsername",
+    },
+    {
+      type: "input",
+      message: "Please enter your contact email for this project",
+      name: "projectEmail",
+    },
+  ])
+  .then(function (data) {
+    console.log(data);
+  });
 
 // function to write README file
 function writeToFile(fileName, data) {}
