540ab272b036b7c0f0063b1e5f0ca7f5066ef5a6
Archangel <nathapcal@gmail.com>
2020-09-24 02:10:31 -0400

    generated the current skeleton for the readme file

--- genReadMe.md
+++ genReadMe.md
@@ -1,3 +1,75 @@
-# project title
+
+  # asdfasdfas
+  <hr>
+  <br/>
+  asdfasdfsa
+  <br/>
+  ## Table of Contents 
+
+  ~[Installation](#installation)
+  ~[Usage](#usage)
+  ~[Contributing](#contributing)
+  ~[Tests](#tests)
+  ~[License](#license)
+  ~[Questions](#questions)
+
+  <br/>
+
+  ## Installation 
   
+  <hr>
+  
+  <br/>
+  
+  asfasdfdsa
+  
+  <br/>
+  
+  ## Usage 
+  
+  <hr>
+  
+  <br/>
+  
+  asdfasfdsda
+  
+    <br/>
+  
+  ## Contributing 
+  
+  <hr>
+  
+  <br/>  
+  
+  #### How to contribute
+  
+  asdfasdfdsa
+  
+  <br/>
+  
+  #### Contributors 
+  
+  asdfasdfdsa
+  
+  <br/>
+  
+  ## Tests
+  
+  <hr>
+  
+  <br/>
+  
+  asdfasdfsa
+  
+  ## License 
+  
+  <hr>
+  
+  <br/>
+  
+  ## Questions
+  
+  <hr>
+  
+  #### If you have any questions or concerns of this project please contact asdfsadfsdaf
   
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -5,8 +5,82 @@
 // }
 
 function generateMarkdown(data) {
-  return `# ${data.projectTitle}
+  return `
+  # ${data.projectTitle}
   
+  <hr>
+  
+  <br/>
+  
+  ${data.projectDescription}
+  
+  <br/>
+
+  ## Table of Contents 
+
+  ~[Installation](#installation)
+  ~[Usage](#usage)
+  ~[Contributing](#contributing)
+  ~[Tests](#tests)
+  ~[License](#license)
+  ~[Questions](#questions)
+
+  <br/>
+
+  ## Installation 
+  
+  <hr>
+  
+  <br/>
+  
+  ${data.projectInstall}
+  
+  
+  ## Usage 
+  
+  <hr>
+  
+  <br/>
+  
+  ${data.projectUsage}
+     
+  ## Contributing 
+  
+  <hr>
+  
+  <br/>  
+  
+  #### How to contribute
+  
+  ${data.projectHowToContribute}
+  
+  <br/>
+  
+  #### Contributors 
+  
+  ${data.projectContributors}
+  
+  <br/>
+  
+  ## Tests
+  
+  <hr>
+  
+  <br/>
+  
+  ${data.projectTest}
+  
+  ## License 
+  
+  <hr>
+  
+  <br/>
+
+  ## Questions
+  
+  <hr>
+  
+  #### If you have any questions or concerns of this project please contact ${data.projectEmail}
   `;
 }
 
